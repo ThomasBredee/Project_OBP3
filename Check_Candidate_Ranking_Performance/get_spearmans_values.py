8d4e4ab0ba137d156f95d4b5d39f3f9cdabeeb88
ThomasBredee <129949732+ThomasBredee@users.noreply.github.com>
2025-02-02 23:54:28 +0100

    Add files via upload

--- Check_Candidate_Ranking_Performance/get_spearmans_values.py
+++ Check_Candidate_Ranking_Performance/get_spearmans_values.py
@@ -8,17 +8,30 @@
 from Input_Transformation.transforming_input import TransformInput
 from Check_Candidate_Ranking_Performance.spearmans_methods import CalcSpearmans
 import pandas as pd
+from Candidate_Ranking.ranking_methods import CandidateRanking
+from VRP_Solver.distance_calculator import RoadDistanceCalculator
+from Check_Candidate_Ranking_Performance.ml_prepper import MLPrepper
 
-FILE_SIZE = "medium"
-METHOD = "haversine"
-file_location = f"VRP_results_{METHOD}_{FILE_SIZE}"
+
+MACHINE_LEARNING = True
+FILE_SIZE = "mini"
+METHOD = "osrm"
+RANKING = "greedy"
+LONG_DEPOT = 5.26860985
+LAT_DEPOT = 52.2517788
+MODEL_TYPE = "xgboost"
+file_location = f"Solve_True_Rankings_VRP/Data_VRP_solved/{METHOD}/VRP_results_{METHOD}_{FILE_SIZE}"
+
 if __name__ == "__main__":
 
     ###Prepare input
     input_df = pd.read_csv(f"Data/{FILE_SIZE}.csv")
-    check_road_proximity = True
+    check_road_proximity = False
+    distance_calc = RoadDistanceCalculator()
     transformer = TransformInput(check_road_proximity=check_road_proximity)
+    input_df = transformer.drop_duplicates(input_df)
     input_df_modified = transformer.execute_validations(input_df)
+    input_df_modified_with_depot = distance_calc.add_depot(input_df_modified, LAT_DEPOT, LONG_DEPOT)
 
     ###Create spearmans matrix
     accuracy = CalcSpearmans()
@@ -26,11 +39,109 @@ if __name__ == "__main__":
     accuracy.correlation_df = pd.DataFrame(index=company_names)
     correlation_df = pd.DataFrame(index=company_names)
 
-    for truck_cap in range(2,21,1):
+    full_matrix = distance_calc.calculate_square_matrix(input_df_modified)
+    if METHOD == "haversine":
+        full_matrix = distance_calc.calculate_square_matrix(input_df_modified)
+    elif METHOD == "osrm":
+        full_matrix = distance_calc.calculate_full_distance_matrix(input_df_modified)
+
+    ranking = CandidateRanking()
+    distance_matrix_for_candidates = ranking.clean_column_and_index_labels(full_matrix)
+    CHOSEN_CANDIDATES = distance_matrix_for_candidates.columns.unique()
+
+    company_data_to_predict = {}
+    company_distance_matrix_vrp = {}
+    prepare_input = MLPrepper()
+    #Loop over each chosen company
+
+    #use SMALL range for machine learning algorithms
+    #for truck_cap in range(2,21,1):
+    for truck_cap in [3, 8, 12, 18]:
+        print('truck_cap',truck_cap)
         sheet_name = f"Ranks_Truck_Cap_{truck_cap}"
-        true_ranking = pd.read_excel(f"Solve_True_Rankings_VRP/Data_VRP_solved/{METHOD}/{file_location}.xlsx", sheet_name=sheet_name, index_col = 0)
-        #accuracy.compute_spearman_greedy(true_ranking, input_df, METHOD, truck_cap)
-        #accuracy.compute_spearman_bounding_circle(true_ranking, input_df, input_df_modified,METHOD ,truck_cap)
-        accuracy.compute_spearman_accuracy_k_means(true_ranking, input_df, input_df_modified,METHOD ,truck_cap)
+        true_ranking = pd.read_excel(f"{file_location}.xlsx", sheet_name=sheet_name, index_col = 0)
+        if MACHINE_LEARNING is False:
+            if RANKING == "greedy":
+                accuracy.compute_spearman_greedy(true_ranking, input_df_modified, METHOD, truck_cap)
+            elif RANKING =="bounding_circle":
+                accuracy.compute_spearman_bounding_circle(true_ranking, input_df, input_df_modified, METHOD, truck_cap)
+            elif RANKING == "k_means":
+                accuracy.compute_spearman_accuracy_k_means(true_ranking, input_df, input_df_modified, truck_cap)
+            elif RANKING =="dbscan":
+                accuracy.compute_spearman_accuracy_dbscan(true_ranking, input_df, input_df_modified, truck_cap)
+
+        if MACHINE_LEARNING is True:
+            for chosen_company in CHOSEN_CANDIDATES:
+                # Filter chosen candidates for the current company
+                company_candidates = list(CHOSEN_CANDIDATES)
+                company_candidates.remove(chosen_company)
+
+                distance_matrix_ranking = full_matrix[full_matrix.index.str.startswith(chosen_company)]
+
+                #change ranking for desired algorithm
+                if RANKING == "greedy":
+                    greedy_ranking = ranking.greedy(distance_matrix_ranking, chosen_company)
+                    data_to_predict_on, distance_matrix_vrp = prepare_input.prep_greedy(input_df_modified_with_depot,
+                                                                                        chosen_company, company_candidates,
+                                                                                        METHOD, distance_matrix_ranking,
+                                                                                        truck_cap, greedy_ranking)
+                    company_data_to_predict[chosen_company] = data_to_predict_on
+                    company_distance_matrix_vrp[chosen_company] = distance_matrix_vrp
+
+                elif RANKING == "bounding_circle":
+                    bounding_ranking = ranking.bounding_circle(input_df, distance_matrix_ranking, chosen_company)
+                    data_to_predict_on, distance_matrix_vrp = prepare_input.prep_bounding_circle(
+                        input_df, input_df_modified, input_df_modified_with_depot, distance_matrix_ranking,
+                        truck_cap, chosen_company, company_candidates, METHOD, bounding_ranking)
+
+                    company_data_to_predict[chosen_company] = data_to_predict_on
+                    company_distance_matrix_vrp[chosen_company] = distance_matrix_vrp
+
+                elif RANKING == "k_means":
+                    k_means_ranking = ranking.k_means(input_df, input_df_modified, full_matrix, chosen_company)
+                    data_to_predict_on, distance_matrix_vrp = prepare_input.prep_k_means(input_df, input_df_modified,
+                                                                                         input_df_modified_with_depot,
+                                                                                         distance_matrix_ranking,
+                                                                                         full_matrix,
+                                                                                         truck_cap, chosen_company,
+                                                                                         CHOSEN_CANDIDATES, METHOD,
+                                                                                         k_means_ranking)
+                    company_data_to_predict[chosen_company] = data_to_predict_on
+                    company_distance_matrix_vrp[chosen_company] = distance_matrix_vrp
+
+                elif RANKING == "dbscan":
+                    EPS = 19*len(input_df)**(-0.1)+5
+                    MS = 2
+                    dbscan_ranking = ranking.dbscan(input_df, full_matrix, chosen_company,EPS, MS)
+                    data_to_predict_on, distance_matrix_vrp = prepare_input.prep_dbscan(input_df, input_df_modified,
+                                                                                        input_df_modified_with_depot,
+                                                                                        distance_matrix_ranking,
+                                                                                        full_matrix, truck_cap,
+                                                                                        chosen_company, CHOSEN_CANDIDATES,
+                                                                                        METHOD, dbscan_ranking, ms=2)
+                    company_data_to_predict[chosen_company] = data_to_predict_on
+                    company_distance_matrix_vrp[chosen_company] = distance_matrix_vrp
+
+            if RANKING == "greedy":
+                accuracy.compute_spearman_accuracy_ml_greedy(true_ranking, truck_cap,
+                                                             RANKING, METHOD, MODEL_TYPE,
+                                                             CHOSEN_CANDIDATES, company_data_to_predict)
+            elif RANKING == "bounding_circle":
+                accuracy.compute_spearman_accuracy_ml_bounding_circle(true_ranking, truck_cap,
+                                                                      RANKING, METHOD, MODEL_TYPE, CHOSEN_CANDIDATES,
+                                                                      company_data_to_predict)
+            elif RANKING == "k_means":
+                accuracy.compute_spearman_accuracy_ml_k_means(true_ranking, truck_cap,
+                                                              RANKING, METHOD, MODEL_TYPE, CHOSEN_CANDIDATES,
+                                                              company_data_to_predict)
+
+
+            elif RANKING == "dbscan":
+                accuracy.compute_spearman_accuracy_ml_dbscan(true_ranking, truck_cap,
+                                                             RANKING, METHOD, MODEL_TYPE, CHOSEN_CANDIDATES,
+                                                             company_data_to_predict)
 
     correlation_df = round(accuracy.correlation_df,3)
+    print(correlation_df)
+    print('mean: ',correlation_df.values.mean())
+    accuracy.perform_test(correlation_df,FILE_SIZE,METHOD)
--- Check_Candidate_Ranking_Performance/spearmans_methods.py
+++ Check_Candidate_Ranking_Performance/spearmans_methods.py
@@ -13,6 +13,7 @@ from Candidate_Ranking.ranking_methods import CandidateRanking
 from VRP_Solver.distance_calculator import RoadDistanceCalculator
 import pandas as pd
 from scipy.stats import spearmanr
+from scipy.stats import kruskal
 
 
 class CalcSpearmans():
@@ -87,7 +88,7 @@ class CalcSpearmans():
 
         self.correlation_df[vehicle_cap] = correlations
 
-    def compute_spearman_accuracy_k_means(self, true_ranking_df, input_df, input_df_modified, method, vehicle_cap):
+    def compute_spearman_accuracy_k_means(self, true_ranking_df, input_df, input_df_modified, vehicle_cap):
         """
         Handle the 'k-means' method for ranking.
         """
@@ -117,42 +118,315 @@ class CalcSpearmans():
 
         self.correlation_df[vehicle_cap]=correlations
 
-    # def accuracy_dbscan(self, df, input_df, input_df_original, j):
-    #     """
-    #     Handle the 'dbscan' method for ranking.
-    #     """
-    #     algorithm = CandidateRanking()
-    #     correlations = []
-    #     calculator = RoadDistanceCalculator()
-    #
-    #     for i in range(len(df)):
-    #         new_df = df.iloc[:, [i]]
-    #         new_df.columns = ['Rankings']
-    #         new_df = new_df[new_df['Rankings'] != len(df)]
-    #         chosen_company = df.columns[i]
-    #         distance_matrix = calculator.calculate_distance_matrix(
-    #             input_df, chosen_company=chosen_company,
-    #             candidate_name=None, method="haversine", computed_distances_df=None
-    #         )
-    #
-    #         # Get full matrix for DBSCAN
-    #         full_matrix = calculator.calculate_square_matrix(input_df)
-    #         ranking = algorithm.dbscan(input_df, full_matrix)
-    #
-    #         # Add ranking and compute Spearman's rank correlation
-    #         ranking_added = self._add_ranking(ranking)
-    #         ranking_df1 = ranking_added['Ranking']
-    #         ranking_df2 = new_df['Rankings']
-    #         correlation, p_value = spearmanr(ranking_df1, ranking_df2)
-    #
-    #         # Print the result
-    #         print(f"Spearman's Rank Correlation: {correlation}")
-    #         print(f"P-value: {p_value}")
-    #         correlations.append(correlation)
-    #
-    #     self.correlation_df[j] = correlations
-    #     return
+
+    def compute_spearman_accuracy_dbscan(self, true_ranking_df, input_df, input_df_modified, vehicle_cap, ms=2):
+        """
+        Computes the Spearman correlation between true and predicted rankings using the DBSCAN clustering method.
+
+        Parameters:
+        - true_ranking_df (DataFrame): The true rankings of candidates for each company.
+        - input_df (DataFrame): The dataset used for clustering.
+        - input_df_modified (DataFrame): A modified version of the dataset, used for distance calculations.
+        - vehicle_cap (int): The vehicle capacity being evaluated.
+        - ms (int, optional): The minimum samples parameter for DBSCAN. Default is 2.
+
+        Process:
+        - Extracts true rankings for each company.
+        - Computes a distance matrix using `RoadDistanceCalculator`.
+        - Determines an appropriate `eps` value for DBSCAN.
+        - Predicts rankings using DBSCAN clustering.
+        - Calculates the Spearman correlation between true and predicted rankings.
+        - Stores correlation results for the given vehicle capacity.
+
+        Returns:
+        - None (results are stored in `self.correlation_df`).
+        """
+        candidate_ranker = CandidateRanking()
+        correlations = []
+        distance_calc = RoadDistanceCalculator()
+
+        for i in range(len(true_ranking_df)):
+            true_ranking_column = true_ranking_df.iloc[:, [i]]
+            chosen_company = true_ranking_column.columns[0]
+            true_ranking_column.columns = ['Rankings']
+            true_ranking_column_without_chosen_comp = true_ranking_column[
+                true_ranking_column['Rankings'] != len(true_ranking_df)]
+
+            # Get full matrix for k-means
+            full_matrix = distance_calc.calculate_square_matrix(input_df_modified)
+            eps = int(round(19 * (len(full_matrix) ** -0.1) + 5, 0))
+            predicted_ranking = candidate_ranker.dbscan(input_df, full_matrix, chosen_company, eps, ms)
+            # Calculate spearmans coeff
+            predicted_ranking_added = self._add_ranking(predicted_ranking)
+            predicted_ranking = predicted_ranking_added['Ranking']
+            true_ranking = true_ranking_column_without_chosen_comp['Rankings']
+            # Reorder ranking
+            predicted_ranking_reordered = predicted_ranking.reindex(true_ranking.index)
+
+            correlation, p_value = spearmanr(predicted_ranking_reordered, true_ranking)
+            correlations.append(correlation)
+
+        self.correlation_df[vehicle_cap] = correlations
 
     def _add_ranking(self, ranking):
+        """
+            Assigns a ranking to each entry in the given DataFrame.
+
+            Parameters:
+            - ranking (DataFrame): The DataFrame containing ranking data.
+
+            Returns:
+            - DataFrame: The input DataFrame with an added 'Ranking' column,
+              where values range from 1 to the length of the DataFrame.
+            """
         ranking['Ranking'] = range(1, len(ranking) + 1)
         return ranking
+
+    def perform_test(self, correlations_df, file_size, method):
+        """
+            Performs the Kruskal-Wallis test to analyze differences in Spearman correlation distributions across truck capacities.
+
+            Parameters:
+            - correlations_df (DataFrame): Spearman correlation values for different truck capacities.
+            - file_size (int): Size of the input data file.
+            - method (str): The ranking method used.
+
+            Outputs:
+            - Prints the Kruskal-Wallis test statistic and p-value.
+            """
+        #Restructure data for kruskal wallis test
+        long_df = correlations_df.reset_index().melt(
+            id_vars='index',
+            var_name='Truck Capacity',
+            value_name='Spearman Coefficient'
+        ).rename(columns={'index': 'Company'})
+
+        #Group data by Truck Capacity
+        groups = [group['Spearman Coefficient'].values for _, group in long_df.groupby('Truck Capacity')]
+
+        #perform Kruskal-Wallis Test
+        kruskal_result = kruskal(*groups)
+        print(f"Kruskal-Wallis Test Result for {method} , and file size {file_size}:")
+        print('statistic:', kruskal_result[0])
+        print('p-value:', kruskal_result[1])
+
+    def compute_spearman_accuracy_ml_greedy(self, true_ranking_df, vehicle_cap,RANKING,METHOD,MODEL_TYPE,CHOSEN_CANDIDATES,company_data_to_predict):
+        """
+            Computes the Spearman correlation between true and predicted rankings using a Greedy-based ML approach.
+
+            Parameters:
+            - true_ranking_df (DataFrame): Actual candidate rankings per company.
+            - vehicle_cap (int): Vehicle capacity.
+            - RANKING, METHOD, MODEL_TYPE (str): Settings for ranking and ML model.
+            - CHOSEN_CANDIDATES (list): List of candidates.
+            - company_data_to_predict (dict): Mapping of companies to their prediction data.
+
+            Updates:
+            - `self.correlation_df` with computed correlations.
+            """
+        candidate_ranker = CandidateRanking()
+        correlations = []
+
+        for i in range(len(true_ranking_df)):
+            true_ranking_column = true_ranking_df.iloc[:, [i]]
+            chosen_company = true_ranking_column.columns[0]
+            true_ranking_column.columns = ['Rankings']
+            true_ranking_column_without_chosen_comp = true_ranking_column[
+                true_ranking_column['Rankings'] != len(true_ranking_df)]
+
+            #prepare data for predictions
+            path = f"Machine_Learning_Train/{METHOD}/TrainedModels/XGBoost/"
+            model_file = f"Machine_Learning_Train/haversine/TrainedModels/XGBoost/xgboost_booster_haversine_greedy.model"
+            scaler_file = f"Machine_Learning_Train/haversine/TrainedModels/XGBoost/scaler_haversine_greedy.pkl"
+
+
+            chosen_candidates = list(CHOSEN_CANDIDATES)
+            chosen_candidates.remove(chosen_company)
+
+            data_to_predict_on = company_data_to_predict[chosen_company]
+
+
+            #Obtain predicted ranking
+            predicted_ranking = candidate_ranker.greedy_ml(data_to_predict_on,model_file,scaler_file,chosen_candidates)
+
+            #Calculate spearmans coeff
+
+            predicted_ranking_added = self._add_ranking(predicted_ranking)
+            predicted_ranking = predicted_ranking_added['Ranking']
+            true_ranking = true_ranking_column_without_chosen_comp['Rankings']
+            #Reorder ranking
+
+            predicted_ranking_reordered = predicted_ranking.reindex(true_ranking.index)
+
+            correlation, p_value = spearmanr(predicted_ranking_reordered, true_ranking)
+            correlations.append(correlation)
+
+        self.correlation_df[vehicle_cap] = correlations
+
+
+    def compute_spearman_accuracy_ml_bounding_circle(self, true_ranking_df, vehicle_cap,RANKING,METHOD,MODEL_TYPE,CHOSEN_CANDIDATES,company_data_to_predict):
+        """
+            Computes the Spearman correlation between true and predicted rankings using a Bounding Circle-based ML approach.
+
+            Parameters:
+            - true_ranking_df (DataFrame): Actual candidate rankings per company.
+            - vehicle_cap (int): Vehicle capacity.
+            - RANKING, METHOD, MODEL_TYPE (str): Settings for ranking and ML model.
+            - CHOSEN_CANDIDATES (list): List of candidates.
+            - company_data_to_predict (dict): Mapping of companies to their prediction data.
+
+            Updates:
+            - `self.correlation_df` with computed correlations.
+            """
+        candidate_ranker = CandidateRanking()
+        correlations = []
+
+        for i in range(len(true_ranking_df)):
+            true_ranking_column = true_ranking_df.iloc[:, [i]]
+            chosen_company = true_ranking_column.columns[0]
+            true_ranking_column.columns = ['Rankings']
+            true_ranking_column_without_chosen_comp = true_ranking_column[
+                true_ranking_column['Rankings'] != len(true_ranking_df)]
+            #prepare data for ml
+            path = f"Machine_Learning_Train/{METHOD}/TrainedModels/XGBoost/"
+            model_file = f"{path}{MODEL_TYPE}_booster_{METHOD}_{RANKING}.model"
+            scaler_file = f"{path}scaler_{METHOD}_{RANKING}.pkl"
+
+
+            chosen_candidates = list(CHOSEN_CANDIDATES)
+            chosen_candidates.remove(chosen_company)
+
+            data_to_predict_on = company_data_to_predict[chosen_company]
+
+            #Obtain predicted ranking
+            predicted_ranking = candidate_ranker.bounding_circle_ml(data_to_predict_on,model_file,scaler_file,chosen_candidates)
+
+
+            # Calculate spearmans coeff
+
+            predicted_ranking_added = self._add_ranking(predicted_ranking)
+            predicted_ranking = predicted_ranking_added['Ranking']
+            true_ranking = true_ranking_column_without_chosen_comp['Rankings']
+
+            #Reorder ranking
+
+            predicted_ranking_reordered = predicted_ranking.reindex(true_ranking.index)
+
+            correlation, p_value = spearmanr(predicted_ranking_reordered, true_ranking)
+            correlations.append(correlation)
+
+        self.correlation_df[vehicle_cap] = correlations
+
+
+
+    def compute_spearman_accuracy_ml_k_means(self,true_ranking_df, vehicle_cap,RANKING,METHOD,MODEL_TYPE,CHOSEN_CANDIDATES,company_data_to_predict):
+        """
+            Computes the Spearman correlation between true and predicted rankings using a K-Means-based ML approach.
+
+            Parameters:
+            - true_ranking_df (DataFrame): Actual candidate rankings per company.
+            - vehicle_cap (int): Vehicle capacity.
+            - RANKING, METHOD, MODEL_TYPE (str): Settings for ranking and ML model.
+            - CHOSEN_CANDIDATES (list): List of candidates.
+            - company_data_to_predict (dict): Mapping of companies to their prediction data.
+
+            Updates:
+            - `self.correlation_df` with computed correlations.
+            """
+        candidate_ranker = CandidateRanking()
+        correlations = []
+
+        for i in range(len(true_ranking_df)):
+            true_ranking_column = true_ranking_df.iloc[:, [i]]
+            chosen_company = true_ranking_column.columns[0]
+            true_ranking_column.columns = ['Rankings']
+            true_ranking_column_without_chosen_comp = true_ranking_column[
+                true_ranking_column['Rankings'] != len(true_ranking_df)]
+
+            # prepare data for ml
+            path = f"Machine_Learning_Train/{METHOD}/TrainedModels/XGBoost/"
+            model_file = f"{path}{MODEL_TYPE}_booster_{METHOD}_{RANKING}.model"
+            scaler_file = f"{path}scaler_{METHOD}_{RANKING}.pkl"
+
+            chosen_candidates = list(CHOSEN_CANDIDATES)
+            chosen_candidates.remove(chosen_company)
+
+            data_to_predict_on = company_data_to_predict[chosen_company]
+
+
+
+            #Obtain predicted ranking
+            predicted_ranking = candidate_ranker.k_means_ml(data_to_predict_on, model_file, scaler_file,
+                                                                    CHOSEN_CANDIDATES)
+
+
+            #Calculate spearmans coeff
+
+            predicted_ranking_added = self._add_ranking(predicted_ranking)
+            predicted_ranking = predicted_ranking_added['Ranking']
+            true_ranking = true_ranking_column_without_chosen_comp['Rankings']
+            #Reorder ranking
+
+            predicted_ranking_reordered = predicted_ranking.reindex(true_ranking.index)
+
+            correlation, p_value = spearmanr(predicted_ranking_reordered, true_ranking)
+            correlations.append(correlation)
+
+        self.correlation_df[vehicle_cap] = correlations
+
+
+    def compute_spearman_accuracy_ml_dbscan(self, true_ranking_df, vehicle_cap,
+                                             RANKING, METHOD, MODEL_TYPE,
+                                             CHOSEN_CANDIDATES, company_data_to_predict):
+        """
+            Computes the Spearman correlation between true and predicted rankings using a DBSCAN-based ML approach.
+
+            Parameters:
+            - true_ranking_df (DataFrame): Actual candidate rankings per company.
+            - vehicle_cap (int): Vehicle capacity.
+            - RANKING, METHOD, MODEL_TYPE (str): Settings for ranking and ML model.
+            - CHOSEN_CANDIDATES (list): List of candidates.
+            - company_data_to_predict (dict): Mapping of companies to their prediction data.
+
+            Updates:
+            - `self.correlation_df` with computed correlations.
+            """
+        candidate_ranker = CandidateRanking()
+        correlations = []
+
+        for i in range(len(true_ranking_df)):
+            true_ranking_column = true_ranking_df.iloc[:, [i]]
+            chosen_company = true_ranking_column.columns[0]
+            true_ranking_column.columns = ['Rankings']
+            true_ranking_column_without_chosen_comp = true_ranking_column[
+                true_ranking_column['Rankings'] != len(true_ranking_df)]
+
+            #prepare data for predictions
+            path = f"Machine_Learning_Train/{METHOD}/TrainedModels/XGBoost/"
+            model_file = f"{path}{MODEL_TYPE}_booster_{METHOD}_{RANKING}.model"
+            scaler_file = f"{path}scaler_{METHOD}_{RANKING}.pkl"
+
+
+            chosen_candidates = list(CHOSEN_CANDIDATES)
+            chosen_candidates.remove(chosen_company)
+
+            data_to_predict_on = company_data_to_predict[chosen_company]
+
+
+            #Obtain predicted ranking
+            predicted_ranking = candidate_ranker.dbscan_ml(data_to_predict_on, model_file, scaler_file,
+                                                            CHOSEN_CANDIDATES)
+
+            #Calculate spearmans coeff
+
+            predicted_ranking_added = self._add_ranking(predicted_ranking)
+            predicted_ranking = predicted_ranking_added['Ranking']
+            true_ranking = true_ranking_column_without_chosen_comp['Rankings']
+            #Reorder ranking
+
+            predicted_ranking_reordered = predicted_ranking.reindex(true_ranking.index)
+
+            correlation, p_value = spearmanr(predicted_ranking_reordered, true_ranking)
+            correlations.append(correlation)
+
+        self.correlation_df[vehicle_cap] = correlations
